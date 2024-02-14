8643e9850db8c56f82923f59b26adb710acc2898
Rafał <rafal.bytniewski@o2.pl>
2024-02-14 21:16:33 +0100

    Added welcome page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+// app.js
+
+import 'bootstrap';
+
+// Import jQuery
+import jQuery from 'jquery';
+
+// Ustawienie jQuery jako globalnej zmiennej
+window.$ = window.jQuery = jQuery;
