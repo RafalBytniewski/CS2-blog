8ee4500e5cc252a8de5bdafe3fdf33724662caea
RafalBytniewski <rafal.bytniewski@o2.pl>
2024-03-09 11:14:25 +0100

    Fixed map show view

--- resources/views/maps/show.blade.php
+++ resources/views/maps/show.blade.php
@@ -14,27 +14,31 @@
         @foreach($grenades as $grenade)
         <div class="card">
             <span>User name: {{$grenade->user->name}}</span>
-            <span>Callout from: {{ $grenade->calloutFrom->name }}</span>
-            <span>Callout to: {{ $grenade->calloutTo->name }}</span>
+            @if(isset($grenade->calloutFrom->name))
+                <span>Callout from: {{ $grenade->calloutFrom->name }}</span>
+            @endif
+            @if(isset($grenade->calloutTo->name))
+                <span>Callout to: {{ $grenade->calloutTo->name }}</span>
+            @endif
             @if($grenade->grenadeImages->count() > 0)
-            <div id="carouselExampleControls{{$grenade->id}}" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    @foreach($grenade->grenadeImages as $key => $image)
-                    <div class="carousel-item {{$key == 0 ? 'active' : ''}}">
-                        <img src="{{ asset('storage/' . $image->path) }}" class="d-block w-100" alt="..."
-                             style="max-width: 540px; height: 960;">
+                <div id="carouselExampleControls{{$grenade->id}}" class="carousel slide" data-bs-interval="false">
+                    <div class="carousel-inner">
+                        @foreach($grenade->grenadeImages as $key => $image)
+                        <div class="carousel-item {{$key == 0 ? 'active' : ''}}">
+                        <img src="{{ asset('storage/' . $image->path) }}" class="mx-auto d-block img-fluid" alt="{{ $grenade->describtion }}"
+    style="max-width: 960px; height: 720px;">
+                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls{{$grenade->id}}" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls{{$grenade->id}}" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls{{$grenade->id}}" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls{{$grenade->id}}" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
             @endif
         </div>
         @endforeach
