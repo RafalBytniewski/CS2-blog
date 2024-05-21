b6d316bf8716ec7023e12c367db0937bd0ff8e36
RafalBytniewski <rafal.bytniewski@o2.pl>
2024-05-21 19:48:32 +0200

    Updated user profile edit page

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -23,56 +23,31 @@
                 <div class="card-header">Account Details</div>
                 <div class="card-body">
                     <form>
-                        <!-- Form Group (username)-->
                         <div class="mb-3">
-                            <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-                            <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="username">
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="nickname">Nickname</label>
+                                <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="username">
+                            </div>   
                         </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (first name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputFirstName">First name</label>
-                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie">
-                            </div>
-                            <!-- Form Group (last name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLastName">Last name</label>
-                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-                            </div>
-                        </div>
-                        <!-- Form Row        -->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (organization name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputOrgName">Organization name</label>
-                                <input class="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap">
-                            </div>
-                            <!-- Form Group (location)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLocation">Location</label>
-                                <input class="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA">
-                            </div>
-                        </div>
-                        <!-- Form Group (email address)-->
+
                         <div class="mb-3">
-                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="name@example.com">
-                        </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (phone number)-->
                             <div class="col-md-6">
-                                <label class="small mb-1" for="inputPhone">Phone number</label>
-                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567">
-                            </div>
-                            <!-- Form Group (birthday)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputBirthday">Birthday</label>
-                                <input class="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988">
+                                <label class="small mb-1" for="inputFirstName">New password</label>
+                                <input class="form-control" id="inputFirstName" type="password" placeholder="Enter your old password" value="">
                             </div>
                         </div>
-                        <!-- Save changes button-->
+                        <div class="mb-3">
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputFirstName">Confirm new password</label>
+                                <input class="form-control" id="inputFirstName" type="password" placeholder="Enter your new password" value="">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputFirstName">Old password</label>
+                                <input class="form-control" id="inputFirstName" type="password" placeholder="Confirm your new password" value="">
+                            </div>
+                        </div>
                         <button class="btn btn-primary" type="button">Save changes</button>
                     </form>
                 </div>
