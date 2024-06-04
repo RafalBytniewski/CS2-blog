07e1215379fd6472dd13419d2150124c948683eb
RafalBytniewski <rafal.bytniewski@o2.pl>
2024-06-04 23:03:59 +0200

    Updated MapSeeder with image_path

--- database/seeders/MapSeeder.php
+++ database/seeders/MapSeeder.php
@@ -14,16 +14,16 @@ class MapSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['name' => 'Mirage','describtion' => 'Map the Mirage','active' => 1],
-            ['name' => 'Nuke', 'describtion' => 'Map the Nuke','active' => 1],
-            ['name' => 'Anubis', 'describtion' => 'Map the Anubis','active' => 1],
-            ['name' => 'Ancient', 'describtion' => 'Map the Ancient','active' => 1],
-            ['name' => 'Overpass', 'describtion' => 'Map the Overpass','active' => 0],
-            ['name' => 'Inferno', 'describtion' => 'Map the Inferno','active' => 1],
-            ['name' => 'Vertigo', 'describtion' => 'Map the Vertigo','active' => 1],
-            ['name' => 'Dust 2', 'describtion' => 'Map the Dust 2','active' => 1],
-            ['name' => 'Office', 'describtion' => 'Map the Office','active' => 0],
-            ['name' => 'Italy', 'describtion' => 'Map the Italy','active' => 0]
+            ['name' => 'Mirage','describtion' => 'Map the Mirage','active' => 1, 'image_path' => ''],
+            ['name' => 'Nuke', 'describtion' => 'Map the Nuke','active' => 1], 'image_path' => '',
+            ['name' => 'Anubis', 'describtion' => 'Map the Anubis','active' => 1, 'image_path' => ''],
+            ['name' => 'Ancient', 'describtion' => 'Map the Ancient','active' => 1, 'image_path' => ''],
+            ['name' => 'Overpass', 'describtion' => 'Map the Overpass','active' => 0, 'image_path' => ''],
+            ['name' => 'Inferno', 'describtion' => 'Map the Inferno','active' => 1, 'image_path' => ''],
+            ['name' => 'Vertigo', 'describtion' => 'Map the Vertigo','active' => 1, 'image_path' => ''],
+            ['name' => 'Dust 2', 'describtion' => 'Map the Dust 2','active' => 1, 'image_path' => ''],
+            ['name' => 'Office', 'describtion' => 'Map the Office','active' => 0, 'image_path' => ''],
+            ['name' => 'Italy', 'describtion' => 'Map the Italy','active' => 0, 'image_path' => '']
         ];  
         Map::insert($data);
     }
