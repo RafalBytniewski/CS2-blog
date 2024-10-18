050517a705a51ac2d663a7e50e1ded02f8d31de2
RafalBytniewski <rafal.bytniewski@o2.pl>
2024-10-18 23:37:13 +0200

    edited vite

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,6 @@ export default defineConfig({
                 'resources/js/mapPageFiltersShow.js',
                 'resources/js/createGrenade.js',
                 'resources/js/mirage.js',
-<<<<<<< HEAD
-=======
-                'resources/css/welcome.css',
->>>>>>> 6520ec6c9924d4f5689095e4e6b23480d3d6fda4
                 'resources/js/sortable.js'
             ],
             refresh: true,
